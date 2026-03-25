7e83117a9e34cef2b814f4202e200ecc8921c7ec
lucassssssouza <lucas.janiszewski.souza@escola.pr.gov.br>
2026-03-25 07:57:45 -0300

    Update desafio 1.py

--- desafio 1.py
+++ desafio 1.py
@@ -1,3 +1,5 @@
+#Digitar o primeiro e segundo numero
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
+#Resultado
 print("A soma é:", num1 + num2)
