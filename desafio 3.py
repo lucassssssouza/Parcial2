b3e249fde3be3165c43d3789ed18dd62a27aad0a
lucassssssouza <lucas.janiszewski.souza@escola.pr.gov.br>
2026-03-25 08:05:05 -0300

    Update desafio 3.py

--- desafio 3.py
+++ desafio 3.py
@@ -1,4 +1,7 @@
+#Nomes pre definidos 
 nomes = ["Lucas", "Gustavo", "Cristian", "Guilherme", "Henrique"]
+#Vai aparecer a lista de nomes
 print("Lista de convidados:")
 for nome in nomes:
     print(f"- {nome}")
+    #Vai aparecer antes de cada nome um Hífen 
