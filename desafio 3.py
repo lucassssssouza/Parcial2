d5b4bbbc3b4f67ba8e54053fabb8b03eafc1c3ea
lucassssssouza <lucas.janiszewski.souza@escola.pr.gov.br>
2026-03-25 08:43:39 -0300

    Update desafio 3.py

--- desafio 3.py
+++ desafio 3.py
@@ -1,7 +1,11 @@
-#Nomes pre definidos 
-nomes = ["Lucas", "Gustavo", "Cristian", "Guilherme", "Henrique"]
-#Vai aparecer a lista de nomes
-print("Lista de convidados:")
+# Aqui digita os nomes 
+n1 = input("Digite o 1º nome: ")
+n2 = input("Digite o 2º nome: ")
+n3 = input("Digite o 3º nome: ")
+n4 = input("Digite o 4º nome: ")
+n5 = input("Digite o 5º nome: ")
+#Aqui irá ter a lista dos nomes pela ordem que antes você digitou
+nomes = [n1, n2, n3, n4, n5]
+print("\nLista de Nomes:")
 for nome in nomes:
     print(f"- {nome}")
-    #Vai aparecer antes de cada nome um Hífen 
