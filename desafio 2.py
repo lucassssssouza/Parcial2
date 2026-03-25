e90180957f73420d88eb11478d04159360b5bc12
lucassssssouza <lucas.janiszewski.souza@escola.pr.gov.br>
2026-03-25 08:02:22 -0300

    Update desafio 2.py

--- desafio 2.py
+++ desafio 2.py
@@ -1,5 +1,9 @@
+#Digitar o numero
 numero = int(input("Digite um número inteiro: "))
+#Calcular os numeros,se a primeira condição for verdadeira ela será executada,e o Else afirma que se o calculo não for 0 (e o resto é 1) ele é executado
 if numero % 2 == 0:
     print(f"O número {numero} é PAR.")
+    #Exibe que o numero é par
 else:
     print(f"O número {numero} é ÍMPAR.")
+    #Exibe que o numero é impar
