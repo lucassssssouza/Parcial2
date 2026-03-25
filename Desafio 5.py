35806b6164b24e0905ef882b20405833e3f2bacc
lucassssssouza <lucas.janiszewski.souza@escola.pr.gov.br>
2026-03-25 08:31:09 -0300

    Create Desafio 5.py

---
+++ Desafio 5.py
@@ -0,0 +1,10 @@
+def main():
+    #Pede os numeros da base e da altura
+    base = int(input('base:'))
+    altura = int (input('altura:'))
+    #Faz o calculo sendo base vezes altura divididos por 2
+    area = (base*altura) /2
+    #Mostra o resultado total da area 
+    print (f'area do triangulo: {area}')
+
+main()
