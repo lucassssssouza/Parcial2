95b8223daf99bd08f6e5a23e27be75b96caece68
lucassssssouza <lucas.janiszewski.souza@escola.pr.gov.br>
2026-03-31 09:31:53 -0300

    Create desafio7.py

---
+++ desafio7.py
@@ -0,0 +1,11 @@
+#  float(input("Digite o Capital (C): "))
+I = float(input("Digite a Taxa de juros em % (I): "))
+T = floColoca-se os dados necessarios para o calculo
+C =at(input("Digite o Tempo (T): "))
+
+# Aqui ocorre a fórmula: J = (C * I * T) / 100
+J = (C * I * T) / 100
+
+# Exibe o resultado com duas casas decimais
+print(f"\nCom um capital de R$ {C:.2f}, taxa de {I}% e tempo de {T},")
+print(f"o valor dos juros (J) é: R$ {J:.2f}")
